53569a6821497ac846b9a627257ef26dd3c4ad9c
drmyersii <drmyersii@users.noreply.github.com>
2015-03-14 14:38:07 -0400

    File names were swapped after recovery... whoops.

--- Vagrantfile
+++ Vagrantfile
@@ -1,27 +1,12 @@
-#!/bin/bash
+VAGRANTFILE_API_VERSION = "2"
 
-printf "Running Vagrant Provisioning..."
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-printf "Updating Box..."
-# make sure the box is fully up to date
-apt-get update
+    # Vagrant box to build on
+    config.vm.box = "ubuntu/trusty64"
 
-# remove below line to allow the system to upgrade
-# apt-get upgrade -y && apt-get dist-upgrade -y
+    # host to guest port forwarding
+    config.vm.network :forwarded_port, guest: 1337, host: 1337
 
-printf "Installing a few necessary packages..."
-# install required packages
-apt-get install -y git nodejs nodejs-legacy npm mongodb-org
-
-# make sure npm is up to date
-npm install -g npm
-
-# remove old hash for npm so bash will find the new version
-hash -d npm
-
-printf "Installing common nodejs tools..."
-# install common nodejs tools
-npm install -g gulp
-npm install -g grunt-cli
-npm install -g bower
-npm install -g yo
+    config.vm.provision :shell, path: "./provisioner.sh"
+end
--- provisioner.sh
+++ provisioner.sh
@@ -1,12 +1,27 @@
-VAGRANTFILE_API_VERSION = "2"
+#!/bin/bash
 
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+printf "Running Vagrant Provisioning..."
 
-    # Vagrant box to build on
-    config.vm.box = "ubuntu/trusty64"
+printf "Updating Box..."
+# make sure the box is fully up to date
+apt-get update
 
-    # host to guest port forwarding
-    config.vm.network :forwarded_port, guest: 1337, host: 1337
+# remove below line to allow the system to upgrade
+# apt-get upgrade -y && apt-get dist-upgrade -y
 
-    config.vm.provision :shell, path: "./provisioner.sh"
-end
+printf "Installing a few necessary packages..."
+# install required packages
+apt-get install -y git nodejs nodejs-legacy npm mongodb-org
+
+# make sure npm is up to date
+npm install -g npm
+
+# remove old hash for npm so bash will find the new version
+hash -d npm
+
+printf "Installing common nodejs tools..."
+# install common nodejs tools
+npm install -g gulp
+npm install -g grunt-cli
+npm install -g bower
+npm install -g yo
