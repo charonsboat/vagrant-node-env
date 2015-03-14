44bf449c01817e2c0eacf73db1e4996849945c25
drmyersii <drmyersii@users.noreply.github.com>
2015-03-14 16:46:57 -0400

    Fixed some formatting on the output text...

--- provisioner.sh
+++ provisioner.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-printf "\nRunning Vagrant Provisioning...\n"
+printf "Running Vagrant Provisioning..."
 
-printf "\nUpdating Box...\n"
+printf "Updating Box..."
 # make sure the box is fully up to date
 apt-get update
 
 # remove below line to allow the system to upgrade
 # apt-get upgrade -y && apt-get dist-upgrade -y
 
-printf "\nAdding MongoDB packages to apt...\n"
+printf "Adding MongoDB packages to apt..."
 # import the public key used by apt
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
@@ -19,7 +19,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.
 # reload the local package database
 apt-get update
 
-printf "\nInstalling a few necessary packages...\n"
+printf "Installing a few necessary packages..."
 # install required packages
 apt-get install -y git nodejs nodejs-legacy npm mongodb-org
 
@@ -29,7 +29,7 @@ npm install -g npm
 # remove old hash for npm so bash will find the new version
 hash -d npm
 
-printf "\nInstalling common nodejs tools...\n"
+printf "Installing common nodejs tools..."
 # install common nodejs tools
 npm install -g gulp
 npm install -g grunt-cli
