15f3a7268cf1057909a981b7d90ef7f7e3577b4e
drmyersii <drmyersii@users.noreply.github.com>
2015-09-25 02:10:29 -0400

    Using Node version 0.* instead of the latest stable release. The latest versions of Node and NPM are causing issues.

--- postinstall.sh
+++ postinstall.sh
@@ -7,7 +7,7 @@ source /home/vagrant/.nvm/nvm.sh
 
 # using latest stable version of node
 # change this to whatever version of node you want to install packages for
-nvm use stable
+nvm use v0.*
 
 # NOTE: Customize the below packages to meet your personal needs
 # install common nodejs tools
